c26111d67daa8d32bd73fd3e139d4997aa53210b
Pavel <ierpvk@gmail.com>
2022-07-15 09:27:26 +0500

    Homework002_1 project

--- Homework002_1/Program.cs
+++ Homework002_1/Program.cs
@@ -1,7 +1,7 @@
 ﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
 и на выходе показывает вторую цифру этого числа.*/
 
-Console.Write("Введите трёхзначное число: ");
+Console.Write("Введи трёхзначное число: ");
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра этого числа: " + stringNumber[1]);
+Console.WriteLine("вторая цифра этого числа -> " + stringNumber[1]);
